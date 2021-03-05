ffd5e238a68e0868fc83b87516ec1e768c336fe4
ericw142 <66480663+ericw142@users.noreply.github.com>
2021-03-05 13:16:59 -0500

    starting upload functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import ImageDisplay from "./components/ImageDisplay";
+import ImageUpload from './components/ImageUpload';
 
 function App() {
   return (
     <div className="App container">
         <ImageDisplay />
+        <hr />
+        <ImageUpload />
     </div>
   );
 }
--- client/src/components/ImageUpload.js
+++ client/src/components/ImageUpload.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function ImageUpload () {
-
+    return(
+        <div>
+            <input type="file" id="fileUpload"></input>
+            <button onclick="s3upload()">Submit</button>
+            <hr />
+            <progress max="100" value="0"></progress>
+        </div>
+    )
 }
 
 export default ImageUpload;
